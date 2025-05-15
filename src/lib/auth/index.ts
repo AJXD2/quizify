81fadd5703e3a0323c088a2d48cfc93e29a67b77
AJXD2 <aj@ajxd2.dev>
2025-05-15 00:13:22 -0400

    Fixed cloudflare

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -4,7 +4,6 @@ import { twoFactor, username } from 'better-auth/plugins';
 import { betterAuth } from 'better-auth';
 import { sendEmail } from '$lib/server/email';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, BETTER_AUTH_SECRET } from '$env/static/private';
-import { PUBLIC_APP_URL } from '$env/static/public';
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg'
@@ -67,7 +66,6 @@ export const auth = betterAuth({
 		}
 	},
 
-	url: PUBLIC_APP_URL,
 	secret: BETTER_AUTH_SECRET
 });
 
